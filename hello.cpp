df0b7b0d5b466bb01f42602a6742503b7e4f9ac8
AmrutaMaliCCTech <103511497+AmrutaMaliCCTech@users.noreply.github.com>
2022-04-13 00:20:08 +0530

    Spelling check for commit
    Corrected the spelling for commit word

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ int main()
     cout << "hello....\n";
     cout<<"How are you ?" << endl;
     cout<<"Looks like my repo is set" << endl;
-    cout<<"Trying the next commint" << endl;
+    cout<<"Trying the next commit" << endl;
     return 0;
 }
