41a370c25c26faaaf0a76514651eea47e169ce00
AmrutaMaliCCTech <103511497+AmrutaMaliCCTech@users.noreply.github.com>
2022-04-13 00:17:17 +0530

    My second commit
    Just trying the commit to see if it works.

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ int main()
     cout << "hello....\n";
     cout<<"How are you ?" << endl;
     cout<<"Looks like my repo is set" << endl;
+    cout<<"Trying the next commint" << endl;
     return 0;
 }
