e27d39673a74c0fa8082968f27e5d91533dc11c4
AmrutaMaliCCTech <103511497+AmrutaMaliCCTech@users.noreply.github.com>
2022-04-13 22:04:50 +0530

    My second program
    This is my second program

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout << "hello....\n";
+    cout << "hello....\n" << endl;
     cout<<"How are you ?" << endl;
     cout<<"Looks like my repo is set" << endl;
     cout<<"Trying the sample commint and something wrong" << endl;
---
+++ my-second-code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("This is my");
+
+    return 0;
+}
