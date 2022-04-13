8b3f7b685cf8be48e4d9e54f9babbada11840ec9
AmrutaMaliCCTech <amruta.mali@cctech.co.in>
2022-04-14 00:03:28 +0530

    This is a trial program commit

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main()
     cout<<"How are you ?" << endl;
     cout<<"Looks like my repo is set" << endl;
     cout<<"Trying the sample commint and something wrong" << endl;
+    cout<<"Some changes" << endl;
     return 0;
 }
